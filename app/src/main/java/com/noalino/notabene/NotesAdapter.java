00fde2be6ec8149116ad29aac172f3dab6321ad4
Benoît G <bgelineau.dev@gmail.com>
2021-10-30 19:16:12 +0200

    Fix notes list layout

--- app/src/main/java/com/noalino/notabene/NotesAdapter.java
+++ app/src/main/java/com/noalino/notabene/NotesAdapter.java
@@ -5,43 +5,48 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder> {
-    private List<NoteBuilder> notesList;
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> {
+    private List<NoteBuilder> localNotesList;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView content;
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
 
-        public MyViewHolder(View view) {
+        public ViewHolder(View view) {
             super(view);
-            content = (TextView) view.findViewById(R.id.content);
+            textView = (TextView) view.findViewById(R.id.content);
+        }
 
+        public TextView getTextView() {
+            return textView;
         }
     }
 
     public NotesAdapter(List<NoteBuilder> notesList) {
-        this.notesList = notesList;
+        localNotesList = notesList;
     }
 
+    @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.note_card, parent, false);
 
-        return new MyViewHolder(itemView);
+        return new ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        NoteBuilder note = notesList.get(position);
-        holder.content.setText(note.getContent());
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        NoteBuilder note = localNotesList.get(position);
+        viewHolder.getTextView().setText(note.getContent());
     }
 
     @Override
     public int getItemCount() {
-        return notesList.size();
+        return localNotesList.size();
     }
 }
